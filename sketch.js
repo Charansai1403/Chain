35f53aa95c721d3f3e1fbec9c7a6288773aa5b1d
Charansai1403 <charansai1403@gmail.com>
2022-06-02 21:13:29 +0530

    modified sketch.js

--- sketch.js
+++ sketch.js
@@ -1,10 +1,13 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var clog,chain;
+
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -33,8 +36,10 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
+    clog = new Log(100,260,300, PI/2);
 
     bird = new Bird(100,100);
+    chain = new Chain(bird.body,clog.body)
 
 }
 
@@ -61,4 +66,6 @@ function draw(){
 
     bird.display();
     platform.display();
+    clog.display();
+    chain.display();
 }
